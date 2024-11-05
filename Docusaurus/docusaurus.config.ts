80c5311c6316771c933f1b1ec781ab7a34f63b0d
Michael Ignasius <144516757+DawnFall19@users.noreply.github.com>
2024-11-05 15:45:08 +0700

    memperbaiki docusaurus.config.ts

--- Docusaurus/docusaurus.config.ts
+++ Docusaurus/docusaurus.config.ts
@@ -67,7 +67,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: '242saurus',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
@@ -81,7 +81,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/DawnFall19/242saurus',
           label: 'GitHub',
           position: 'right',
         },
@@ -125,7 +125,7 @@ const config: Config = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/DawnFall19/242saurus',
             },
           ],
         },
